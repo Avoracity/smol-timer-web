5aef2e129282ba27bea432bae1fb37db89351093
Avoracity <malvarez@venturseed.com>
2023-08-14 15:58:00 -0400

    randomize color when timer is added

--- timer-app/src/App.js
+++ timer-app/src/App.js
@@ -1,15 +1,24 @@
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 import CircularProgressTimer from './CircularProgressTimer';
 
 const App = () => {
   const [timers, setTimers] = useState([]);
-  const inputRefs = useRef([]); // Refs for input fields
+  //const inputRefs = useRef([]); // Refs for input fields
 
   const addTimer = () => {
     const newTimer = { id: Date.now() };
     setTimers([...timers, newTimer]);
   };
 
+  function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+  //let randomColor = getRandomColor();
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-indigo-950">
       <h1 className="text-3xl font-bold mb-4 text-yellow-50">Circular Progress Timers</h1>
@@ -20,13 +29,16 @@ const App = () => {
         Add Timer
       </button>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {timers.map((timer, index) => (
-          <div key={timer.id}>
-            <div className="rounded-lg p-4 shadow-md bg-indigo-900">
-              <CircularProgressTimer ref={(ref) => (inputRefs.current[index] = ref)} />
+        {timers.map((timer, index) => {
+          const randomColor = getRandomColor();
+          return (
+            <div key={timer.id}>
+              <div className="rounded-lg p-4 shadow-md bg-indigo-900" style={{ boxShadow: `6px 6px 0px 0px ${randomColor}` }}>
+                <CircularProgressTimer />
+              </div>
             </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
     </div>
   );
